376c4f164e691a2c854e14a507d7bdf439a3808b
César Augusto Ventura <cesar.ventura.lima@gmail.com>
2022-02-19 12:38:02 -0300

    termo

--- termo.py
+++ termo.py
@@ -1,6 +1,28 @@
 import turtle, random
 lista_de_palavras = ['sagaz', 'negro', 'amago', 'exito', 'mexer', 'termo', 'senso', 'nobre', 'algoz', 'afeto',
-                     'plena', 'etica', 'mutua', 'sutil', 'tenue', 'vigor', 'aquem', 'audaz', 'porem', 'fazer']
+                     'plena', 'etica', 'mutua', 'sutil', 'tenue', 'vigor', 'aquem', 'audaz', 'porem', 'fazer',
+                     'sanar', 'inato', 'assim', 'cerne', 'desde', 'ideia', 'fosse', 'poder', 'moral', 'torpe',
+                     'honra', 'justo', 'muito', 'vasco', 'anexo', 'gozar', 'futil', 'razao', 'quiça', 'icone',
+                     'etnia', 'sobre', 'tange', 'egide', 'lapso', 'mutuo', 'sonho', 'expor', 'haver', 'habil',
+                     'casal', 'amigo', 'posse', 'pesar', 'tempo', 'avido', 'ardil', 'entao', 'boçal', 'corja',
+                     'genro', 'coser', 'seara', 'dengo', 'prole', 'detem', 'causa', 'dizer', 'tenaz', 'digno',
+                     'dever', 'crivo', 'obice', 'apice', 'saber', 'porra', 'ansia', 'brado', 'ceder', 'gleba',
+                     'animo', 'paria', 'assaz', 'atroz', 'comum', 'graça', 'culto', 'temor', 'orgia', 'sendo',
+                     'censo', 'mundo', 'denso', 'pauta', 'fugaz', 'valha', 'cozer', 'nenem', 'ainda', 'vicio',
+                     'reves', 'forte', 'vulgo', 'pudor', 'ester', 'regra', 'dogma', 'louco', 'banal', 'criar',
+                     'pifio', 'tenro', 'impor', 'desse', 'apraz', 'reaça', 'jeito', 'ordem', 'atras', 'pedir',
+                     'round', 'clava', 'saude', 'viril', 'usura', 'merce', 'manso', 'juizo', 'sabio', 'ontem',
+                     'servo', 'prosa', 'presa', 'feliz', 'afago', 'fluir', 'coisa', 'falar', 'cunho', 'forma',
+                     'devir', 'meiga', 'vendo', 'serio', 'plato', 'homem', 'guisa', 'pleno', 'temer', 'visar',
+                     'bruma', 'cisma', 'limbo', 'mesmo', 'magoa', 'xibiu', 'acaso', 'puder', 'gerar', 'exodo',
+                     'heroi', 'obvio', 'afins', 'obter', 'valor', 'impio', 'lugar', 'matiz', 'praxe', 'crise',
+                     'senil', 'havia', 'venia', 'fluxo', 'enfim', 'certo', 'alibi', 'tedio', 'ritmo', 'reter',
+                     'tomar', 'posso', 'grato', 'uniao', 'vital', 'valia', 'visao', 'favor', 'abrir', 'bravo',
+                     'todos', 'laico', 'ameno', 'facil', 'genio', 'reles', 'obito', 'possa', 'olhar', 'falso',
+                     'levar', 'tesao', 'burro', 'selar', 'fator', 'festa', 'sexta', 'judeu', 'monge', 'rumor',
+                     'piada', 'roupa', 'chefe', 'nuvem', 'ambar', 'bença', 'clero', 'amiga', 'demao', 'natal',
+                     'nessa', 'sigla', 'firma', 'rente', 'enjoo', 'zumbi', 'ousar', 'santo', 'lutar', 'baixo',
+                     'coroa', 'forro', 'macho', 'farto', 'ultra', 'lento', 'harem']
 resposta = random.choice(lista_de_palavras) #para escolher uma palavra da lista acima
 y = 250
 print(resposta)
@@ -18,20 +40,26 @@ def draw_square(x, y, cor):   #cordenadas x e y e cores
 
 def input_guess(prompt):
     chute = turtle.textinput("Palavra de 5 Letras", prompt)
-    if chute == None: return "       "  #se cancelar ou se aceitar com nada
-    elif len(chute) != 5: return chute[0:5]  #envia as 5 primeiras letras
-    else: return chute.lower()
+    if chute == None:
+        return "       "  #se cancelar ou se aceitar com nada
+    elif len(chute) != 5:
+        return chute[0:5]  #envia as 5 primeiras letras
+    else:
+        return chute.lower()
 
 def check_guess(chute, resposta, y):
     count = 0
     x = -250        #localização do x
     for i in chute:
-        if i == resposta[count]: draw_square(x, y, "green")     #letra certa desenha um quadrado verde
-        elif i in resposta: draw_square(x, y, "yellow")
-        else: draw_square(x, y, "red")
+        if i == resposta[count]:
+            draw_square(x, y, "green")     #letra certa desenha um quadrado verde
+        elif i in resposta:
+            draw_square(x, y, "yellow")      #letra certa em lugar errado desenha quadrado amarelo
+        else:
+            draw_square(x, y, "red")     #letra errada
         count += 1
         x += 75     #move a coordenada 75
-    turtle.penup()
+    turtle.penup()      #penup desativa a caneta
     turtle.goto(x, y - 25)
     turtle.write(chute, font=("Verdana", 15, "normal"))
 
